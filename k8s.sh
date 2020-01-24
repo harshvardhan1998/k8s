1ccb16ae8d1083dad30af1a08197b0fe12743b4f
harshvardhan1998 <harshvardhan7162@gmail.com>
2020-01-24 19:03:46 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-sudo su
+
  
 #kubelet requires swap off
-swapoff -a
+sudo swapoff -a
 
 # Install Docker CE
 ## Set up the repository:
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install   apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get update && sudo apt-get install   apt-transport-https ca-certificates curl software-properties-common -y
 
 ### Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 ### Add Docker apt repository.
-add-apt-repository \
+sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
 
 ## Install Docker CE.
-apt-get update && apt-get install -y  \
+sudo apt-get update && sudo apt-get install -y  \
   containerd.io=1.2.10-3 \
   docker-ce=5:19.03.4~3-0~ubuntu-$(lsb_release -cs) \
   docker-ce-cli=5:19.03.4~3-0~ubuntu-$(lsb_release -cs)
 
 # Setup daemon.
-cat > /etc/docker/daemon.json <<EOF
+sudo cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -37,23 +37,23 @@ cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 
-mkdir -p /etc/systemd/system/docker.service.d
+sudo mkdir -p /etc/systemd/system/docker.service.d
 # Restart docker.
-systemctl daemon-reload
-systemctl restart docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
 
 
 #Installing kubeadm, kubelet and kubectl
-apt-get update &&  apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
+sudo apt-get update &&  sudo apt-get install -y apt-transport-https curl
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
 
 cat <<EOF |  tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
