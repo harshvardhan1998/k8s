d38daaf1908222b70722ef5329c0c34287d92b2a
harshvardhan1998 <harshvardhan7162@gmail.com>
2020-01-24 09:55:45 +0530

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -43,10 +43,10 @@ systemctl restart docker
 
 
 #Installing kubeadm, kubelet and kubectl
-apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get update &&  apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg |  apt-key add -
 
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+cat <<EOF |  tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
