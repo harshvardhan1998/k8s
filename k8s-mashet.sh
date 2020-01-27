322c44b2c6be35e4549f802adef0714a9d78bd85
harshvardhan1998 <harshvardhan7162@gmail.com>
2020-01-27 10:53:05 +0530

    Update k8s-mashet.sh

--- k8s-mashet.sh
+++ k8s-mashet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo kubeadm init --control-plane-endpoint 172.31.44.139:6443 --upload-certs --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
 
